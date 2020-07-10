ca48f4129488a96b27215caecc04798c270be8e3
Alessandro9300 <aandreoli93@gmail.com>
2020-07-10 15:29:48 +0200

    mappe inserite nel db

--- boolbnb_project/public/js/geocoding.js
+++ boolbnb_project/public/js/geocoding.js
@@ -94,35 +94,35 @@
 /***/ (function(module, exports) {
 
 $(document).ready(function () {
-  var streetName = $(".streetName").val();
-  var streetNumber = $(".streetNumber").val();
-  var municipality = $(".municipality").val();
-  $.ajax({
-    url: 'https://api.tomtom.com/search/2/structuredGeocode.json?key=HPIuQNQKJvFfEyPKVEciiGGYx8Fs3ptB&countryCode=it&streetNumber=<streetNumber>&streetName=<streetName>&municipality=<municipality>',
-    data: {
-      streetNumber: streetNumber,
-      streetName: streetName,
-      municipality: municipality
-    },
-    method: 'get',
-    success: function success(data) {
-      console.log('sono la latitudine = ' + data.results[0].position.lat + 'sono la longitudine = ' + data.results[0].position.lon);
-      var longitude = data.results[0].position.lon;
-      var latitude = data.results[0].position.lat;
-      var position = [longitude, latitude]; // document.getElementById("latitude").innerHTML =  latitude ;
-
-      $('#latitude').val(latitude);
-      $('#longitude').val(longitude);
-    },
-    error: function error(_error) {
-      console.log('Ciao sono un errore');
-    }
-  });
   $('#form').submit(function (e) {
-    // console.log('ecco i dati');
+    var streetName = $(".streetName").val();
+    var streetNumber = $(".streetNumber").val();
+    var municipality = $(".municipality").val();
+    $.ajax({
+      url: 'https://api.tomtom.com/search/2/structuredGeocode.json?key=HPIuQNQKJvFfEyPKVEciiGGYx8Fs3ptB&countryCode=it&streetNumber=<streetNumber>&streetName=<streetName>&municipality=<municipality>',
+      data: {
+        streetNumber: streetName,
+        streetName: streetNumber,
+        municipality: municipality
+      },
+      method: 'get',
+      success: function success(data) {
+        console.log('sono la latitudine = ' + data.results[0].position.lat + 'sono la longitudine = ' + data.results[0].position.lon);
+        var longitude = data.results[0].position.lon;
+        var latitude = data.results[0].position.lat;
+        var position = [longitude, latitude]; // document.getElementById("latitude").innerHTML =  latitude ;
+
+        $('#latitude').val(latitude);
+        $('#longitude').val(longitude);
+      },
+      error: function error(_error) {
+        console.log('errore dati');
+      }
+    }); // console.log('ecco i dati');
+
     var route = $('#form').data('route');
     var form_data = $(this);
-    $.ajax({
+    setTimeout($.ajax({
       headers: {
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
       },
@@ -131,9 +131,25 @@ $(document).ready(function () {
       method: "post",
       success: function success(Response) {},
       error: function error(_error2) {
-        console.log('cè un errore');
+        console.log('errore controller');
       }
-    });
+    }), 3000); // $.ajax({
+    //   headers: {
+    //     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    //   } ,
+    //   url: route,
+    //   data: form_data.serialize(),
+    //   method: "post",
+    //   success: function (Response){
+    //
+    //     console.log(Response);
+    //
+    //   },
+    //   error: function(error){
+    //     console.log('errore controller');
+    //   }
+    // })
+
     e.preventDefault();
   });
 });
