c2f0338084b6df05c5e2415d40fe08cfc7a20e5a
Matteo <Rosella.matteo@gmail.com>
2020-07-10 17:15:53 +0200

    Profilo

--- boolbnb_project/resources/views/profilo.blade.php
+++ boolbnb_project/resources/views/profilo.blade.php
@@ -3,17 +3,46 @@
 
 
 @section('content')
-  <a href="{{route("create-apartment")}}"><button type="button" name="button">Inserisci appartamento</button></a>
 
+<div class="container">
+  <div class="profilo">
+    <div class="top">
+      <div class="floatl">
+        <h1>Benvenuto, ecco la lista degli appartamenti da lei inseriti</h1>
+      </div>
+      <div class="floatr">
 
-  <ul>
+        <a href="{{route("create-apartment")}}"><button type="button" name="button">INSERISCI APPARTAMENTO</button></a>
+      </div>
+
+    </div>
+
     @foreach ($user -> apartments as $apartment)
-      <li>
-        {{$apartment -> image}} {{$apartment -> title}} -> <a href="{{route('stats', $apartment -> id)}}">Statistiche</a>
-        <a href="{{route('show-apartment', $apartment -> id)}}">mio apartment</a>
-      </li>
+      <div class="appartamento">
+        <div class="titolo">
+          <h2>{{$apartment -> title}}</h2>
+
+        </div>
+        <div class="immagine">
+          <img src="{{$apartment -> image}}" alt="">
+        </div>
+        <div class="scritte">
+          <div class="bottoni">
+            <a href="{{route('delete-apartment', $apartment['id'])}}"><button type="button" name="button" class="right red">Elimina</button></a>
+            <a href="{{ route('edit-apartment', $apartment -> id ) }}"><button type="button" name="button" class="right ">Modifica</button></a>
+          </div>
+          <div class="pi">
+            <p><a href="{{route('stats', $apartment -> id)}}">Statistiche Appartamento</a></p>
+            <p><a href="{{route('show-apartment', $apartment -> id)}}">Dati Appartamento</a></p>
+          </div>
+
+        </div>
+
+      </div>
     @endforeach
-  </ul>
+  </div>
+</div>
+
 
 
 @endsection
