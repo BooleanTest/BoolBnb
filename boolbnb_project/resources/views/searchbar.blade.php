38c51fd95714964166b686e409f516ad8fb316d7
Emanuele <em.zaccaria@gmail.com>
2020-07-13 09:51:20 +0200

    Ritorno a stato precedente di "Prova barra di ricerca"

--- boolbnb_project/resources/views/searchbar.blade.php
+++ boolbnb_project/resources/views/searchbar.blade.php
@@ -1,19 +1,21 @@
+<div class="search-h-bar">
 
-  <div class="search-h-bar">
-    <form class="search-h-form" action="" method="post">
+  <form class="search-h-form" action="" method="post">
 
-      <div class="search-h-box">
-        <label for="">
-          <input type="text" name="" value="" placeholder="Dove vuoi andare?">
-        </label>
-        <div id="search-button">
-          <a href="#">
-            <i class="fas fa-plane fa-4x"></i>
-          </a>
-        </div>
+    <div class="search-h-box">
+
+      <label for="">
+        <input id="h-search-input" type="text" name="h-search-input" value="" placeholder="Dove vuoi andare?">
+      </label>
+
+      <div id="search-button">
+        <a href="#">
+          <i class="fas fa-plane fa-4x"></i>
+        </a>
       </div>
 
-    </form>
-  </div>
+    </div>
+
+  </form>
 
 </div>
--- boolbnb_project/resources/views/welcome.blade.php
+++ boolbnb_project/resources/views/welcome.blade.php
@@ -6,10 +6,11 @@
     <title>Classe11_Team7</title>
     <link rel="stylesheet" href="/css/app.css">
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital@1&family=Fondamento&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/18da76cc27.js" crossorigin="anonymous"></script>
   </head>
   <body>
-    
+
     @include ('header')
 
 
