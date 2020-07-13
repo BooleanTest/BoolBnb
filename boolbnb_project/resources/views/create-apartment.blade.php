cefc0c34d88abdcd0cdb509f1437c358efb10d05
Alessandro9300 <aandreoli93@gmail.com>
2020-07-13 15:31:34 +0200

    fix merge

--- boolbnb_project/resources/views/create-apartment.blade.php
+++ boolbnb_project/resources/views/create-apartment.blade.php
@@ -1,11 +1,8 @@
 @include('header')
 @extends('layouts.app')
-
 @section('content')
-
 <div class="containeredit">
   <h2>Inserisci Appartamento</h2>
-
   @if ($errors->any())
   @foreach ($errors->all() as $error)
   <p>{{$error}}</p>
@@ -14,7 +11,6 @@
   <form id='form' action="{{route('store-apartment')}}"  method='post'>
     @csrf
     @method('post')
-<<<<<<< Updated upstream
     <div class="dati">
       <h3>Dati Appartamento</h3>
       <div class="valore">
@@ -95,75 +91,10 @@
         </div>
         <input type="file" name="image" value="{{ old('image')}}"><br>
       </div>
-
     </div>
     <div class="servizi">
       <h2>Servizi</h2>
       <label for="services[]"></label> <br>
-=======
-
-    <label for="title">Titolo</label>
-    <input type="text" name="title" value="{{ old('title')}}">
-
-    <br>
-
-    <label for="rooms">Stanze</label>
-    <input type="number" name="rooms" value="{{ old('rooms')}}">
-
-    <br>
-
-    <label for="bathrooms">Bagni</label>
-    <input type="number" name="bathrooms" value="{{ old('bathrooms')}}">
-
-    <br>
-
-    <label for="meters">Metri quadrati</label>
-    <input type="number" name="meters" value="{{ old('meters')}}">
-
-    <br>
-
-    <label for="nation">Nazione</label>
-    <input type="text" name="nation" value="{{ old('nation')}}">
-
-    <br>
-
-    <label for="city">Città</label>
-    <input type="text" class="municipality" name="city" value="{{ old('city')}}">
-
-    <br>
-
-    <label for="address">Indirizzo</label>
-    <input type="text" class="streetName" name="address" value="{{ old('address')}}">
-
-    <br>
-
-    <label for="number">Numero civico</label>
-    <input type="text" class="streetNumber" name="number" value="{{ old('number')}}">
-
-    <br>
-
-    <label for="image">Immagine</label>
-    <input type="text " name="image" value="{{ old('image')}}"><br>
-
-    <button type="button" id="calcolo" name="calcolo">Calcola</button><br>
-
-    <label for="latitude">latitude</label>
-    <input id="latitude" type="text" name="latitude" value=""><br>
-
-
-
-    <label for="longitude">longitude</label>
-    <input id="longitude" type="text" name="longitude" value="">
-    <br> <br>
-
-    <p id='plong'></p>
-    <p id='plat'></p>
-
-
-
-
-    <label for="services[]">Servizi</label> <br>
->>>>>>> Stashed changes
       @foreach ($services as $service)
       <div class="servizietto">
         <span>{{$service -> name}}</span>
@@ -171,24 +102,11 @@
       </div>
       @endforeach
     </div>
-
-
-
     <div class="bottoni">
       <a href="{{route('home')}}"><button type="button" name="button" class="red">Indietro</button></a>
       <a href="#"><button  id='button' type="submit" name="submit">Salva</button></a>
     </div>
-
-
-
-
-
   </form>
-
   <script src="{{ asset('js/geocoding.js') }}" defer></script>
-
 </div>
-
-
-
 @endsection
