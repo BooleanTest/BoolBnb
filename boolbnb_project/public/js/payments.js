2496ea59268dfecfe26bcb0141875ab6fe660d1e
Emanuele <em.zaccaria@gmail.com>
2020-07-16 09:47:27 +0200

    Merge: creazione file payment.js (per ora inutilzzato)

--- boolbnb_project/public/js/payments.js
+++ boolbnb_project/public/js/payments.js
@@ -81,121 +81,30 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 4);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./resources/js/search.js":
-/*!********************************!*\
-  !*** ./resources/js/search.js ***!
-  \********************************/
+/***/ "./resources/js/payments.js":
+/*!**********************************!*\
+  !*** ./resources/js/payments.js ***!
+  \**********************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-$(document).ready(function () {
-  $('#query').keyup(function () {
-    var query = $('#query').val();
-    console.log(query);
-    $.ajax({
-      url: 'https://api.tomtom.com/search/2/structuredGeocode.json?key=HPIuQNQKJvFfEyPKVEciiGGYx8Fs3ptB&countryCode=it&municipality=<municipality>',
-      data: {
-        municipality: query
-      },
-      method: 'get',
-      success: function success(data) {
-        var longitude = data.results[0].position.lon;
-        var latitude = data.results[0].position.lat;
-        var position = [longitude, latitude];
-        console.log(latitude, longitude);
-        $('#lat').val(latitude);
-        $('#long').val(longitude);
-      },
-      error: function error(_error) {
-        $('#error').text('non esiste alcun campo');
-      }
-    });
-  });
-  var d = new Date();
-  var n = d.getTime();
-  console.log(n); // var route = $('.search-h-form').data('search');
-  //
-  // console.log(route);
-  //
-  //  $('#search-button').submit(function(e){
-  //
-  //   e.preventDefault();
-  //
-  //   var query = $('#h-search-input').val();
-  //   var route = $('.search-h-form').data('search');
-  //   var form_data = $(this);
-  //   console.log(query);
-  //
-  //   $.ajax({
-  //     headers: {
-  //       'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-  //     } ,
-  //     url: route,
-  //     data: form_data.serialize(),
-  //     method: "post",
-  //     success: function (Response){
-  //       console.log(Response);
-  //
-  //     },
-  //     error: function(error){
-  //       console.log('errore controller');
-  //     }
-  //   })
-  //
-  //   e.preventDefault();
-  //
-  //
-  //
-  //
-  // })
-  //   jQuery(document).ready(function(){
-  //
-  //     var route = $('.search-h-form').data('search');
-  //
-  //             jQuery('#search-button').click(function(e){
-  //                e.preventDefault();
-  //                // $.ajaxSetup({
-  //                //    headers: {
-  //                //         'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
-  //                //    }
-  //               // });
-  //                $.ajax({
-  //                   // headers: {
-  //                   //   'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-  //                   // },
-  //                   url: route,
-  //                   method: 'post',
-  //                   data: {
-  //                      city: jQuery('#city').val()
-  //                   },
-  //                   success: function(result){
-  //                      console.log('ciao');
-  //                   },
-  //                   error: function(error){
-  //                     console.log(error);
-  //                   }
-  //                 });
-  //                });
-  //             });
-  // //
-  //
-});
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open 'D:\\Booleans\\BoolBnb\\boolbnb_project\\resources\\js\\payments.js'");
 
 /***/ }),
 
-/***/ 2:
-/*!**************************************!*\
-  !*** multi ./resources/js/search.js ***!
-  \**************************************/
+/***/ 4:
+/*!****************************************!*\
+  !*** multi ./resources/js/payments.js ***!
+  \****************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\Booleans\BoolBnb\boolbnb_project\resources\js\search.js */"./resources/js/search.js");
+module.exports = __webpack_require__(/*! D:\Booleans\BoolBnb\boolbnb_project\resources\js\payments.js */"./resources/js/payments.js");
 
 
 /***/ })
